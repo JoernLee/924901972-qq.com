2b18c3b5cd157fb7db4354fa9c5c8dc8c9caae31
JoernLee <924901972@qq.com>
2020-05-04 12:28:22 +0800

    feat:实现深拷贝Merge策略

--- src/core/mergeConfig.ts
+++ src/core/mergeConfig.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig } from '../types'
+import { deepMerge, isPlainObject } from '../helpers/util'
 
 // 定义合并策略的Map，通过Key找到值
 const strats = Object.create(null)
@@ -16,11 +17,33 @@ function fromVal2Strat(val1: any, val2: any) {
   }
 }
 
+// 因为val有可能是对象或者一个普通值
+function deepMergeStrat(val1: any, val2: any): any {
+  if (isPlainObject(val2)) {
+    // 如果val2是对象，返回一个深拷贝的merge
+    return deepMerge(val1, val2)
+  } else if (typeof val2 !== 'undefined') {
+    // val2不是对象，是普通值，直接使用val2就好了
+    return val2
+  } else if (isPlainObject(val1)) {
+    return deepMerge(val1)
+  } else if (typeof val1 !== 'undefined') {
+    return val1
+  }
+}
+
 // 定义不同key并将对应策略存入
 const stratKeysFromVal2 = ['url', 'params', 'data']
 
 stratKeysFromVal2.forEach(key => {
   strats[key] = fromVal2Strat
+})
+
+// headers需要使用深度Merge策略
+const stratKeysDeepMerge = ['headers']
+
+stratKeysDeepMerge.forEach(key => {
+  strats[key] = deepMergeStrat
 })
 
 // 将两个config合并
--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -21,8 +21,35 @@ export function isPlainObject(val: any): val is Object {
 export function extend<T, U>(to: T, from: U): T & U {
   for (const key in from) {
     // 明确类型，避免报错
-    (to as T & U)[key] = from[key] as any
+    ;(to as T & U)[key] = from[key] as any
   }
   return to as T & U
+}
 
+// 我们可以自己实现一个简化版深拷贝
+// lodash里面深拷贝太复杂,暂不需要
+// 通常这种工具函数可以利用单元测试，多尝试Case看返回是否正确
+export function deepMerge(...objs: any[]): any {
+  const result = Object.create(null)
+  // 支持多个参数，所以多一层循环就行了
+  objs.forEach(obj => {
+    if (obj) {
+      Object.keys(obj).forEach(key => {
+        const val = obj[key]
+        // 如果值还是对象，需要递归处理,否则直接赋值就行
+        if (isPlainObject(val)) {
+          // 这里还需要处理该key是否已经存在的情况
+          // 因为objs是两个多个输入
+          if (isPlainObject(result[key])) {
+            result[key] = deepMerge(result[key], val)
+          } else {
+            result[key] = deepMerge(val)
+          }
+        } else {
+          result[key] = val
+        }
+      })
+    }
+  })
+  return result
 }
