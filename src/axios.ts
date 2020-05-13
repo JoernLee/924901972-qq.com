766dc7d82c67c7253c07ce11e6e88f767f3c3653
Joern <924901972@qq.com>
2020-05-13 21:58:25 +0800

    feat: 扩展axios静态属性

--- src/axios.ts
+++ src/axios.ts
@@ -3,6 +3,8 @@ import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaults from './defaults'
 import mergeConfig from './core/mergeConfig'
+import CancelToken from './cancel/CancelToken'
+import Cancel, { isCancel } from './cancel/Cancel'
 
 function createInstance(config: AxiosRequestConfig): AxiosStatic {
   // 拿到之前定义的axios实例
@@ -22,5 +24,10 @@ const axios = createInstance(defaults)
 axios.create = function create(config) {
   return createInstance(mergeConfig(defaults, config))
 }
+
+// 扩展Cancel相关静态方法
+axios.CancelToken = CancelToken
+axios.Cancel = Cancel
+axios.isCancel = isCancel
 
 export default axios
